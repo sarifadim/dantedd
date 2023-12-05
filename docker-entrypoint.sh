d0dc3d7f8f80baffd717415ed80db6c01a4f2109
sarifadim <115786763+sarifadim@users.noreply.github.com>
2023-12-05 16:46:22 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,5 +20,14 @@ case $1 in
         exec sockd
     ;;
 esac
-
+sleep 3
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+./ngrok authtoken 1x6OZh4ADJKXesAGiiogK1RwEsD_3Pk2AYEdbtxJAjNswFAHR
+sleep 3
+nohup ./ngrok tcp 443 &>/dev/null &
+sleep 3
+apk add --no-cache curl
+sleep 3
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 
